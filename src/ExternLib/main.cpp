2886a4ea0e38a70c6edc89cb15720c3a60f94829
EliasROUSSEAU <elrousseau@gaming.tech>
2025-01-28 09:29:26 +0100

    Migration lib externe fini et fonctionnelle

--- src/ExternLib/main.cpp
+++ src/ExternLib/main.cpp
@@ -1,8 +1,2 @@
 #include "pch.h"
 #include "main.h"
-
-int main()
-{
-    std::cout << "Hello, World!\n";
-    return 0;
-}
--- src/ExternLib/main.h
+++ src/ExternLib/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#pragma once
+class main
+{
+};
 
-
-
-#endif
